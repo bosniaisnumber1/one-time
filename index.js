e2848a127938a00c89c1eb38ee8e5d10a93417f4
bosniaisnumber1 <bosniaisnumber1letsgo@gmail.com>
2024-12-01 23:09:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ app.get('/hi', (req, res) => {
 });
 
 
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+// const root = require("path").join(__dirname, "build");
+// app.use(express.static(root));
+// app.get("*", (req, res) => {
+//     res.sendFile("index.html", { root });
+// });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`);
+});
+
 // app.get('/', (req, res) => {
 //   const path = resolve(process.env.STATIC_DIR + '/index.html');
 //   res.sendFile(path);
