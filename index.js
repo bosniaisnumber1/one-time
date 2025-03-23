b72c6850c86504eacd135fae041fc99864a9038a
bosniaisnumber1 <bosniaisnumber1letsgo@gmail.com>
2025-03-22 20:14:25 -0400

    Add for express checkout

--- index.js
+++ index.js
@@ -153,6 +153,19 @@ app.post('/webhook', async (req, res) => {
   res.sendStatus(200);
 });
 
+app.post('/create-intent', async (req, res) => {
+  console.log('testOutRes', res);
+  const intent = await stripe.paymentIntents.create({
+    // To allow saving and retrieving payment methods, provide the Customer ID.
+    customer: customer.id,
+    amount: 50,
+    currency: 'gbp',
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {enabled: true},
+  });
+  res.json({client_secret: intent.client_secret});
+});
+
 app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
 
 
